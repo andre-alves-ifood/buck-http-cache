dda9c1aa4d20022345fa99e7583d171cca70f2ae
andre-alves-ifood <andre.alves@ifood.com.br>
2020-07-11 09:57:26 -0300

    JVM & Ignite performance tips

--- run_buck_cache_client.sh
+++ run_buck_cache_client.sh
@@ -6,4 +6,15 @@ rm -rf ignite
 
 ./gradlew distJar
 
-$JAVA_HOME/bin/java -Xmx16g -Xms512m -XX:+UseG1GC -XX:MaxGCPauseMillis=100 -XX:MaxDirectMemorySize=6g -Dlog.home=/var/log/buck-cache-client/logs -jar cache/build/libs/cache-1.0.0-standalone.jar server cache/src/dist/config/$1.yml
+$JAVA_HOME/bin/java \
+    -Xmx8g \
+    -Xms8g \
+    -XX:+AlwaysPreTouch \
+    -XX:+UseG1GC \
+    -XX:+ScavengeBeforeFullGC \
+    -XX:+DisableExplicitGC \
+    -XX:MaxGCPauseMillis=200 \
+    -XX:MaxDirectMemorySize=6g \
+    -Dlog.home=/var/log/buck-cache-client/logs \
+    -jar cache/build/libs/cache-1.0.0-standalone.jar \
+    server cache/src/dist/config/"$1".yml
