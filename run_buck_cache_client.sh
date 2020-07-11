98571acf733cff993db304a4aaac298baf0b7a61
andre-alves-ifood <andre.alves@ifood.com.br>
2020-07-11 09:24:33 -0300

    Delete directory before starting ignite

--- run_buck_cache_client.sh
+++ run_buck_cache_client.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Remove ignite's default workspace
+# Use this only when WAL is disabled
+rm -rf ignite
+
 ./gradlew distJar
 
 $JAVA_HOME/bin/java -Xmx16g -Xms512m -XX:+UseG1GC -XX:MaxGCPauseMillis=100 -XX:MaxDirectMemorySize=6g -Dlog.home=/var/log/buck-cache-client/logs -jar cache/build/libs/cache-1.0.0-standalone.jar server cache/src/dist/config/$1.yml
